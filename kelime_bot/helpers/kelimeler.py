b2bf02ca59341d726b479e932105b9364f5ad93c
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-08-08 18:14:25 +0400

    Update kelimeler.py

--- kelime_bot/helpers/kelimeler.py
+++ kelime_bot/helpers/kelimeler.py
@@ -1,5 +1,5 @@
 import random
-kelimeler = ["gözəl ", "məlumat ", "sual", "sağlam ", "buraxmaq", "yaraşıqlı", "su", "etmək", "işlədmək", "demək", "görmək",
+kelimeler = ["gözəl", "məlumat", "sual", "sağlam", "buraxmaq", "yaraşıqlı", "su", "etmək", "işlədmək", "demək", "görmək",
              "yenidən", "həssas", "sanki", "manat", "oynamaq", "çiçək", "şəhər", "yüksəlmək", "çabalamaq", "varlıq", "diyar",
              "güvən", "gərək", "müalicə", "bir", "rahat", "soyuq", "hava", "kitab", "paylaşmaq", "hesap", "vücud",
              "torpaq", "kitab", "sistem", "xoşallanmaq", "çəkilmək", "texnik", "yaxınlaşmaq", "il", "tarix", "dəqiq", "qaqaş",
